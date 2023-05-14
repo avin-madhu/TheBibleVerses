b19e891e6d45d153910f7a68d88a419dcf3870f1
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2023-05-14 21:33:08 +0530

    sections

--- BibleVerses/script.js
+++ BibleVerses/script.js
@@ -1,3 +1,31 @@
+
+const id = "specific-verse"
+
+function scrollInto(id) {
+  var elmnt = document.getElementById(id);
+  elmnt.scrollIntoView({
+      behavior: "smooth", 
+      block: "start",
+      inline: "nearest"
+  });
+}
+
+function showSpecSection(){
+  const sectionSpec = document.getElementById("specific-verse");
+  sectionSpec.style.display = "block";
+  const sectionHome = document.getElementById("home")
+  sectionHome.style.display = "none"
+  scrollInto(id);
+}
+
+function back(){
+  const sectionSpec = document.getElementById("specific-verse");
+  sectionSpec.style.display = "none";
+  const sectionHome = document.getElementById("home")
+  sectionHome.style.display = "block"
+}
+
+
 async function getMeVerse() {
 
   const book = document.getElementById("book").value;
@@ -5,28 +33,23 @@ async function getMeVerse() {
   const verse = parseInt(document.getElementById("verse").value);
   const verseText = document.getElementById("verseText");
   const fromText = document.getElementById("fromText");
-  const cite = document.getElementById("cite");
-
+  
   try {
-    // Make an HTTP GET request to the Bible API
-    const response = await fetch(`https://bible-api.com/${book}${chapter}:${verse}`);
 
-    // Parse the JSON response
+    const response = await fetch(`https://bible-api.com/${book}${chapter}:${verse}`);
     const data = await response.json();
 
-    // Display the verse text to the user
     console.log(data.text)
     
-
     verseText.innerText = data.text;
     fromText.innerText = `${book} ${chapter}:${verse}`;
   
-
-
   } catch (error) {
-    // Log any errors to the console
+
     console.error(error);
   }
+
+  
 
 }
 
